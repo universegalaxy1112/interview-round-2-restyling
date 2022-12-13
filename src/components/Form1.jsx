d236bb7ea8f7d594d07a8015312dfba1ea4e27ef
universegalaxy1112 <54145818+universegalaxy1112@users.noreply.github.com>
2022-12-13 16:32:32 +0800

    Add ant library

--- src/components/Form1.jsx
+++ src/components/Form1.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Button } from 'antd';
 
 const Form1 = () => {
   return (
     <div className={'top-container'}>
-
+      <Button type="primary">Button</Button>
     </div>
   );
 }
