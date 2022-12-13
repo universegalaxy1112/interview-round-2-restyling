ef6fbab39035b08786f37526843c572769f209f1
universegalaxy1112 <54145818+universegalaxy1112@users.noreply.github.com>
2022-12-13 22:48:39 +0800

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 import Form1 from "./components/Form1";
+import { ConfigProvider, Layout } from "antd";
+const { Content } = Layout;
 
 function App() {
   return (
-    <div className={'top-container'}>
-      <Form1 />
-    </div>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#6dabe4",
+          fontFamily: "Poppins"
+        },
+      }}
+    >
+      <Layout className='layout'>
+        <Content style={{ padding: "150px 0px" }}>
+          <Form1 />
+        </Content>
+      </Layout>
+    </ConfigProvider>
   );
 }
 
--- src/components/Form1.jsx
+++ src/components/Form1.jsx
@@ -1,12 +1,83 @@
-import React from 'react';
-import { Button } from 'antd';
+import React from "react";
+import { Row, Col, theme, Typography, Form, Button, Checkbox } from "antd";
+import { LockOutlined, UserOutlined, MailOutlined, LockFilled } from "@ant-design/icons";
+
+import signUpImage from "../assets/images/signup-image.jpg";
+const { Title } = Typography;
 
 const Form1 = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <div className={'top-container'}>
-      <Button type="primary">Button</Button>
+    <div className='container' style={{ background: colorBgContainer }}>
+      <Row>
+        <Col sm={24} md={12} className={"form-container"}>
+          <Title level={1} strong={true} className='form-title'>
+            Sign up
+          </Title>
+          <Form
+            name='basic'
+            method='POST'
+            initialValues={{ remember: true }}
+            wrapperCol={{ span: 16 }}
+            className='register-form'
+            id='register-form'
+          >
+            <div className='form-group'>
+              <label htmlFor='name'>
+                <UserOutlined />
+              </label>
+              <input type='text' name='name' id='name' placeholder='Your Name' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='email'>
+                <MailOutlined />
+              </label>
+              <input type='email' name='email' id='email' placeholder='Your Email' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='pass'>
+                <LockFilled />
+              </label>
+              <input type='password' name='pass' id='pass' placeholder='Password' />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='re-pass'>
+                <LockOutlined />
+              </label>
+              <input type='password' name='re_pass' id='re_pass' placeholder='Repeat your password' />
+            </div>
+            <div className='form-group'></div>
+            <Form.Item name='disabled' valuePropName='checked'>
+              <Checkbox>
+                I agree all statements in{" "}
+                <Typography.Link className='term-service' underline>
+                  {" "}
+                  Terms of service
+                </Typography.Link>
+              </Checkbox>
+            </Form.Item>
+
+            <div className='form-group form-button'>
+              <Button type='primary' size={"medium"} className={"form-submit"}>
+                Register
+              </Button>
+            </div>
+          </Form>
+        </Col>
+        <Col sm={24} md={12} className={"image-container"}>
+          <figure>
+            <img src={signUpImage} alt='register image' />
+          </figure>
+          <Typography.Link className='signup-image-link' underline>
+            I am already member
+          </Typography.Link>
+        </Col>
+      </Row>
     </div>
   );
-}
+};
 
 export default Form1;
